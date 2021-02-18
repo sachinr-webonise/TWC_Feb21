0f4906f3b212fc0203b13dd01a8e733707ac448c
sachinr-webonise <sachin.raina@weboniselab.com>
2021-02-18 18:03:25 +0530

    Total banaya 18th feb 2021

---
+++ StockManagSystem.java
@@ -0,0 +1,12 @@
+package TWC;
+
+public class StockManagSystem {
+
+	
+	
+	
+	
+	public void uploadProductFile() {
+		System.out.println("Welcome Sachin\nPlease provide the location of the stock file ");
+	}
+}
---
+++ localBanya.java
@@ -0,0 +1,13 @@
+package TWC;
+
+public class localBanya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StockManagSystem sms=new StockManagSystem();
+		sms.uploadProductFile();
+		
+		
+	}
+
+}
